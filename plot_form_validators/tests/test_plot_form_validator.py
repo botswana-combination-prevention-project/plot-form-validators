e4a5787f1d1e4babd5c1435f4af5216e12455ad3
erikvw <ew2789@gmail.com>
2017-06-17 17:00:35 +0200

    remove unused code

--- plot_form_validators/tests/test_plot_form_validator.py
+++ plot_form_validators/tests/test_plot_form_validator.py
@@ -230,61 +230,6 @@ class TestSpecialLocations(TestCase):
             form_validator.validate)
         self.assertIn('special_location', form_validator._error_codes)
 
-# class TestForms(PlotTestMixin, TestCase):
-#
-#     def test_add_ess_plot_form(self):
-#         plot = mommy.prepare_recipe(
-#             'plot.plot',
-#             ess=True,
-#             status=RESIDENTIAL_HABITABLE,
-#             time_of_week='weekdays',
-#             time_of_day='morning',
-#             household_count=1,
-#             eligible_members=1,
-#             gps_confirmed_latitude=fake.confirmed_latitude,
-#             gps_confirmed_longitude=fake.confirmed_longitude,
-#         )
-#         form = PlotForm(data=plot.__dict__)
-#         self.assertTrue(form.is_valid())
-#
-#     def test_cannot_add_non_ess_plot_form(self):
-#         plot = mommy.prepare_recipe(
-#             'plot.plot',
-#             ess=False,
-#             status=RESIDENTIAL_HABITABLE,
-#             time_of_week='weekdays',
-#             time_of_day='morning',
-#             household_count=1,
-#             eligible_members=1,
-#             gps_confirmed_latitude=fake.confirmed_latitude,
-#             gps_confirmed_longitude=fake.confirmed_longitude,
-#         )
-#         form = PlotForm(data=plot.__dict__)
-#         self.assertFalse(form.is_valid())
-#
-#     def test_cannot_add_non_ess_non_residential_plot_form(self):
-#         plot = mommy.prepare_recipe(
-#             'plot.plot',
-#             ess=True,
-#             status=RESIDENTIAL_NOT_HABITABLE,
-#             gps_confirmed_latitude=fake.confirmed_latitude,
-#             gps_confirmed_longitude=fake.confirmed_longitude,
-#         )
-#         form = PlotForm(data=plot.__dict__)
-#         self.assertFalse(form.is_valid())
-#
-#     def test_plot_log_entry_form(self):
-#         """Add a plot log entry.
-#         """
-#         plot = self.make_confirmed_plot(household_count=1)
-#         plot_log = PlotLog.objects.get(plot=plot)
-#         form = PlotLogEntryForm(
-#             data=dict(
-#                 plot_log=plot_log.id,
-#                 report_datetime=self.get_utcnow(),
-#                 log_status=ACCESSIBLE))
-#         self.assertTrue(form.is_valid())
-#
 #     def test_attempt_to_add_many_plot_log_entry_per_day(self):
 #         """Attempt to add more than one plot log entry in a day.
 #         """
